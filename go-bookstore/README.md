6777c2fe5ab0c845e75d9e9f692d48c4e1ca63ca
Ahmad Muslih Zain <ahmad.muslih@gmail.com>
2022-05-09 18:49:51 +0300

    add project 3

---
+++ go-bookstore/README.md
@@ -0,0 +1,3 @@
+TODO:
+
+- [ ] Fix pkg/config/app.go line 15 `panic: default addr for network 'admin' unknown`
---
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/aemzayn/11-go-projects/go-bookstore/pkg/models"
+	"github.com/aemzayn/11-go-projects/go-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	UpdateBook := &models.Book{}
+	utils.ParseBody(r, UpdateBook)
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, db := models.GetBookById(Id)
+	if UpdateBook.Name != "" {
+		bookDetails.Name = UpdateBook.Name
+	}
+	if UpdateBook.Author != "" {
+		bookDetails.Author = UpdateBook.Author
+	}
+	if UpdateBook.Publication != "" {
+		bookDetails.Publication = UpdateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
